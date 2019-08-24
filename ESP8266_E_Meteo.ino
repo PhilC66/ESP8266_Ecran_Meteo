3f175ab5b5b3eaef52366079a569bd8570215453
PhilC66 <moncasot@gmail.com>
2019-08-24 12:19:05 +0200

    maj des librairies, sauf conservé sinon bug compil? Adafruit_GFX_Library version 1.3.6 Adafruit_ILI9341 version 1.2.0 Adafruit_STMPE610 version 1.0.1

--- ESP8266_E_Meteo.ino
+++ ESP8266_E_Meteo.ino
@@ -93,7 +93,7 @@ struct config_t                    // configuration sauvée en EEPROM
 } config;
 
 const String soft = "ESP8266_E_Meteo.ino.adafruit"; 	// nom du soft
-const int 	 ver  = 106;
+const int 	 ver  = 107;
 
 const byte nbrVille	= 5;
 String ville[3][nbrVille + 1] = {
